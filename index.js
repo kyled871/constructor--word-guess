c3a0a89966df612d7e99240fdc37245c405cfc8e
Kyle Davis <kyled871@yahoo.com>
2020-06-16 14:43:46 -0400

    added random word func

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ let wordBank = [
     'chinchilla',
 ]
 
+function randomPick() {
+    let randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    console.log(randomWord)
+
+    
+}
+
+randomPick()
