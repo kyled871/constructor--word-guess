b73ece9c5562a14bb42b3765807d542ec06c860e
Kyle Davis <kyled871@yahoo.com>
2020-06-16 18:22:41 -0400

    added more functions

--- Word.js
+++ Word.js
@@ -2,20 +2,21 @@ let Letter = require("./Letter")
 
 function Word(currentWord) {
 
-    this.wordArr = []
-    this.currentWord = currentWord
-
+    
     this.newLetter = currentWord.split("").map( function(val) {
         return new Letter(val)
     })
-
+    
     this.show = function() {
+        
+        this.wordArr = []
+        
 
         for (let i = 0; i < this.newLetter.length; i++) {
             this.wordArr.push(this.newLetter[i].hasGuessed())
         }
 
-        console.log(this.wordArr.join("   "))
+        console.log(this.wordArr.join(" "))
 
     }
 }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let Word = require("./Word")
-let inquirer = require('inquirer');
+const Word = require("./Word")
+const Letter = require("./Letter");
+const inquirer = require('inquirer');
 
 let guessesLeft = 10
 
@@ -14,36 +15,80 @@ let wordBank = [
 ]
 
 function gameStart() {
-
     let randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-    let current = new Word(randomWord)
-
+    let current = new Word(randomWord);
+    let eachLetter = new Letter(current.newLetter)
+    
+    let guessesLeft = 10
+    console.log(`Guesses Left: ${guessesLeft}`)
+    current.show()
+    console.log(`\n`)
     question()
 
-}
+    function question() {
+    
+        if (guessesLeft > 0) {
+            inquirer.prompt([
+    
+                {
+                    type: 'input',
+                    name: 'userInput',
+                    message: 'Please select a letter: \n'
+                }
+    
+            ]).then(function(answer) {
+                
+                let wrong = true
+                let guess = answer.userInput
+                let letterCount = 0
+    
+                for (i = 0; i < eachLetter.letter.length; i++) {
+                    if (guess === eachLetter.letter[i].letter) {
 
-
-function question() {
-
-    if (guessesLeft != 0) {
-
-        inquirer.prompt([
-
-            {
-                type: 'input',
-                name: 'userInput',
-                message: 'Please select a letter: '
-            }
-
-        ])
-
-
-
+                        eachLetter.letter[i].guessed = true
+                        wrong = false
+                        ++letterCount
+                    }      
+                } 
+    
+                if (wrong) {
+                    --guessesLeft
+                    wrong = true
+                } else {
+                    console.log(`Correct!\n`)
+                }
+    
+                console.log(`Guesses Left: ${guessesLeft}`) 
+                console.log(`\n`)   
+                current.show()
+                console.log(`\n`)
+                question()
+    
+            })
+    
+        } else {
+            inquirer.prompt([
+                {
+                    type: 'confirm',
+                    name:'startOver',
+                    message: 'Game Over! Try Again?',
+                    default: true
+    
+                }
+            ]).then( function(answer) {
+    
+                if (answer.startOver) {
+                    guessesLeft = 10
+                    gameStart()
+    
+                }
+    
+            })
+        }
+        
     }
-
-
 }
-
-
 
 gameStart()
+
+
